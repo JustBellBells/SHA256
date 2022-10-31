132926d5f8ffa98f49ef6ffc1bd304cb98f958d6
bells <bells@justbells.net>
2022-10-31 01:12:08 -0400

    Set up project & added readme

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,7 @@
 #include <iostream>
-#include <string>
 
-void hash();
-void dehash();
 
 int main()
 {
-    int option = 0;
-    std::cout << "[1]: hash string" << std::endl << "[2]: dehash string" << std::endl;
-    std::cin >> option;
-    switch(option)
-    {
-        case(1): hash();
-        case(2): dehash();
-    }
-}
 
-void hash()
-{
-    std::string inputString;
-    std::cout << "Input string to hash: ";
-    std::cin >> inputString;
-    std::cout << inputString;
-}
-
-void dehash()
-{
-    std::string inputHash;
-    std::cout << "Input hash to dehash: ";
-    std::cin >> inputHash;
-    std::cout << inputHash;
 }
